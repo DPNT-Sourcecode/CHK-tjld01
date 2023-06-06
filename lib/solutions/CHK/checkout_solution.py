73517b9e67edab16483c6b874b61cdd254322e2a
sbx_user1051 <sbx_user1051@169.254.27.9>
2023-06-06 17:01:24 +0000

    Tue Jun 06 17:01:24 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,9 +4,10 @@ def checkout(skus):
     sum = 0
     item_dict = {}
     for item in skus:
-        if item not in "ABCDE":
+        if item not in "ABCDEF":
             return -1
         item_dict[item] = item_dict.get(item, 0) + 1
+
 
     # Reduce Quantity of Free Items
     if item_dict.get("E",0) >= 2 and item_dict.get("B",0) > 0:
@@ -14,6 +15,7 @@ def checkout(skus):
         item_dict["B"] = item_dict["B"] - to_reduce if to_reduce <= item_dict["B"] else 0
     if item_dict.get("F",0) >= 3:
         to_reduce = item_dict["F"]//3
+        item_dict["F"] -= to_reduce
 
     # Calculate Checkout
     for item, quant in item_dict.items():
@@ -28,11 +30,11 @@ def checkout(skus):
         elif item == "E":
             sum += quant * 40
         elif item == "F":
-            sum += quant*10
+            sum += quant * 10
 
     return sum
 
-print(checkout("FFFFFFF"))
+
 
 
 
