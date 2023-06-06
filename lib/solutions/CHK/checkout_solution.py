39e7b5e225759298cda4e7623fd3f5940b27342c
sbx_user1051 <sbx_user1051@169.254.27.9>
2023-06-06 16:49:41 +0000

    Tue Jun 06 16:49:41 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,21 +6,12 @@ def checkout(skus):
     for item in skus:
         if item not in "ABCDE":
             return -1
-        if item == "A":
-            item_dict["A"] = item_dict.get("A", 0) + 1
-        elif item == "B":
-            item_dict["B"] = item_dict.get("B", 0) + 1
-        elif item == "C":
-            item_dict["C"] = item_dict.get("C", 0) + 1
-        elif item == "D":
-            item_dict["D"] = item_dict.get("D", 0) + 1
-        elif item == "E":
-            item_dict["E"] = item_dict.get("E", 0) + 1
+        item_dict[item] = item_dict.get(item, 0) + 1
 
     # Reduce Quantity of Free Items
-    # if item_dict["E"] >= 2 and item_dict["B"] > 0:
-    #     to_reduce = item_dict["E"]//2
-    #     item_dict["B"] = item_dict["B"] - to_reduce if to_reduce <= item_dict["B"] else 0
+    if item_dict["E"] >= 2 and item_dict["B"] > 0:
+        to_reduce = item_dict["E"]//2
+        item_dict["B"] = item_dict["B"] - to_reduce if to_reduce <= item_dict["B"] else 0
 
     # Calculate Checkout
     for key, quant in item_dict.items():
@@ -36,4 +27,7 @@ def checkout(skus):
             sum += quant * 40
 
     return sum
+
+print(checkout("BBBBBBBBBBB"))
+
 
