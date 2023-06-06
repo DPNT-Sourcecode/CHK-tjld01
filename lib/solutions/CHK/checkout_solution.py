01ea76265cc0f0aecf786ff1fdad4a625ce1c7a9
sbx_user1051 <sbx_user1051@169.254.27.9>
2023-06-06 17:28:26 +0000

    Tue Jun 06 17:28:26 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -137,46 +137,41 @@ prices ={
     }
 }
 
-print(prices)
 
-item_dict = {}
-total_price = 0
-pattern = r"[A-Z]"
 def checkout(skus):
+    item_dict = {}
+    total_price = 0
+    sku_pattern = r"[A-Z]"
+
+    # Count occurenses of each item
     for item in skus:
-        item_exist = re.match(pattern, item)
+        item_exist = re.match(sku_pattern, item)
         if not item_exist:
             return -1
         item_dict[item] = item_dict.get(item, 0) + 1
 
-
-    # Reduce Quantity of Free Items
-    if item_dict.get("E",0) >= 2 and item_dict.get("B",0) > 0:
-        to_reduce = item_dict["E"]//2
-        item_dict["B"] = item_dict["B"] - to_reduce if to_reduce <= item_dict["B"] else 0
-    if item_dict.get("F",0) >= 3:
-        to_reduce = item_dict["F"]//3
-        item_dict["F"] -= to_reduce
-
-    # Calculate Checkout
+    # Calculate total price based on prices and special offers
     for item, quant in item_dict.items():
         if item == "A":
-            sum += (quant // 5) * 200 + ((quant % 5) // 3)*130 + ((quant % 5) % 3) * 50
+            total_price += (quant // 5) * 200 + ((quant % 5) // 3)*130 + ((quant % 5) % 3) * 50
         elif item == "B":
-            sum += (quant // 2) * 45 + (quant % 2) * 30
+            total_price += (quant // 2) * 45 + (quant % 2) * 30
         elif item == "C":
-            sum += quant * 20
+            total_price += quant * 20
         elif item == "D":
-            sum += quant * 15
+            total_price += quant * 15
         elif item == "E":
-            sum += quant * 40
+            total_price += quant * 40
         elif item == "F":
-            sum += quant * 10
+            total_price += quant * 10
 
-    return sum
+
+    return total_price
+
 
 
 print(checkout("AAAAA"))
+
 
 
 
