4f4cf534ebaa22303b708aaf2e5ac0dd397792d2
sbx_user1051 <sbx_user1051@169.254.27.9>
2023-06-06 16:15:10 +0000

    Tue Jun 06 16:15:10 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    print(skus)
-    return skus
+    sum = 0
+    item_dict = {}
+    for item in skus:
+        if item not in "ABCD":
+            return -1
+        if item == "A" and not in item_dict:
+            item_dict["A"] = 1
+        if item == "B":
+            ret
+
